4e9754caa706886881560136a77cb48a063903c8
SwarnaKumari <50829003+SwarnaKumari@users.noreply.github.com>
2022-11-22 14:31:48 +0530

    Add files via upload

---
+++ Employee.kt
@@ -0,0 +1,12 @@
+package modal
+
+
+//decalred a class with primary constructor
+//in model class we do what variable declaration
+class Employee (id:Int,name:String,salary:Int) {
+
+    var id:Int=id
+    var name:String=name
+    var salary:Int=salary
+
+}
